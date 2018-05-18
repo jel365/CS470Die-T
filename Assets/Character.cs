f347b1639f8e871a5da38fd580423c296ebf7b7e
jel365 <jameselee@cpp.edu>
2018-05-18 12:45:54 -0700

    Add files via upload

---
+++ Assets/Character.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+    MeshRenderer m;
+    Color selected;
+    Color old;
+    Mover master;
+    public GameObject control;
+    void Start()
+    {
+        m = GetComponent<MeshRenderer>();
+        m.material.color = Color.black;
+        selected = Color.blue;
+        old = m.material.color;
+
+        master = control.GetComponent<Mover>();
+    }
+
+    void OnMouseOver()
+    {
+        m.material.color = selected;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            master.SetActivePlayer(gameObject);
+        }
+
+    }
+
+    void OnMouseExit()
+    {
+
+        m.material.color = old;
+    }
+}
